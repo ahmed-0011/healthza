dd16876222a7209c9f8ec28d00fbe47bbf055838
yazan <yazan.nidal@hotmail.com>
2021-03-18 01:58:49 +0200

    ADD TEST /1

---
+++ app/src/main/java/com/example/healthza/AddGlucoseTest.java
@@ -0,0 +1,415 @@
+package com.example.healthza;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class AddGlucoseTest extends AppCompatActivity implements View.OnClickListener
+        ,CompoundButton.OnCheckedChangeListener
+        , View.OnFocusChangeListener
+{
+
+    private static final  String ChannelID= "AddGlucoseTestNote";
+
+    CheckBox autoTD;
+    ImageView dateI;
+    ImageView timeI;
+
+    TextView datE;
+    TextView timE;
+    TextView td;
+
+    private EditText glucose;
+
+    private Button clear;
+    private Button add;
+
+    public boolean onSupportNavigateUp()
+    {
+        Log.w ("Add Glucose test.", "onSupportNavigateUp is calll");
+        onBackPressed ();
+        return super.onSupportNavigateUp ();
+    }
+    //
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        //complet
+        glucose.clearFocus();
+        return super.onKeyDown(keyCode, event);
+    }
+    //
+    @Override
+    public void onBackPressed()
+    {
+        //super.onBackPressed ();
+        Log.w ("Add Glucose test.", "this onbackpress is calll");
+
+        AlertDialog.Builder   x= new AlertDialog.Builder ( this );
+        x.setMessage ( "DO YOU WANT TO EXIT?" ).setTitle ( "Exit Activity'Add Glucose test'" )
+
+                .setPositiveButton ( "YES_EXIT", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.w ("Add Glucose test.", "end");
+                        Toast.makeText(getApplicationContext(), "Back...", Toast.LENGTH_SHORT).show();
+                        //complet
+                        finish();
+                    }
+                } )
+
+                .setNegativeButton ( "CANCEL", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { }
+                })
+
+                .setIcon(R.drawable.qus)
+                .setPositiveButtonIcon (getDrawable ( R.drawable.yes))
+                .setNegativeButtonIcon(getDrawable ( R.drawable.no))
+                .show ();
+        return;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        //complet
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_glucose_test);
+
+        Log.w ("Add Glucose test.", "start");
+        Toast.makeText(getApplicationContext(), "Add Glucose test....", Toast.LENGTH_SHORT).show();
+
+        ActionBar bar = getSupportActionBar ();
+        bar.setHomeButtonEnabled ( true );
+        bar.setDisplayHomeAsUpEnabled ( true );
+        bar.setHomeAsUpIndicator ( R.drawable.ex);
+        bar.setTitle("Add Glucose test.");
+
+        datE = findViewById(R.id.dateText0);
+        timE = findViewById(R.id.timeText0);
+        td = findViewById(R.id.textView);
+
+        datE.setText("YYYY/MM/DD");
+        timE.setText("HH:MM");
+
+        dateI = findViewById(R.id.DateIcon0);
+        dateI.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "ٌٌSet Date...", Toast.LENGTH_SHORT).show();
+                showDatePickerDialog();
+                //complet
+            }
+        });
+
+        timeI = findViewById(R.id.TimeIcon0);
+        timeI.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Set Time...", Toast.LENGTH_SHORT).show();
+                showTimePickerDialog();
+                //complet
+            }
+        });
+
+        autoTD = findViewById(R.id.TimeDateAuto0);
+        autoTD.setChecked(false);
+        autoTD.setOnClickListener(this);
+        td.setVisibility(View.VISIBLE);
+
+        glucose = findViewById(R.id.innerGlucosePercent);
+        glucose.setOnFocusChangeListener(this);
+
+        clear = findViewById(R.id.ClearGlucoseTest); clear.setOnClickListener (this);
+        add = findViewById(R.id.AddGlucoseTest); add.setOnClickListener(this);
+
+        //complet
+    }
+
+    //Date Picker
+    public void showDatePickerDialog() {
+        Functions.DatePickerFragment.setYear(0); Functions.DatePickerFragment.setMonth(0); Functions.DatePickerFragment.setDay(0);
+        DialogFragment newFragment = new Functions.DatePickerFragment(datE);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+        newFragment = null;
+    }
+
+    //Time Picker
+    public void showTimePickerDialog() {
+        Functions.TimePickerFragment.setHour(0); Functions.TimePickerFragment.setMinute(0);
+        DialogFragment newFragment = new Functions.TimePickerFragment(timE);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+        newFragment = null;
+    }
+
+    //time and date auto
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        if (autoTD.equals(view)) {
+            if (checked) {
+                timeI.setEnabled(false);
+                dateI.setEnabled(false);
+                td.setVisibility(View.INVISIBLE);
+
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/mm/dd hh:mm:ss");
+                LocalDateTime now = LocalDateTime.now();
+                Functions.timeS = now.getHour()+":"+now.getMinute();
+                Functions.dateS = now.getYear()+"/"+now.getMonthValue()+"/"+now.getDayOfMonth();
+                timE.setText(Functions.timeS);
+                datE.setText(Functions.dateS);
+            } else {
+                timeI.setEnabled(true);
+                dateI.setEnabled(true);
+                td.setVisibility(View.VISIBLE);
+            }
+
+            // TODO: Veggie sandwich
+        }
+    }
+
+    //Empty Fields
+    boolean ifEmptyFields()
+    {
+        //complet
+        boolean empty = false;
+        empty = empty || timE.getText().toString().equals("HH:MM");
+        empty = empty || datE.getText().toString().equals("YYYY/MM/DD");
+        return empty || glucose.getText().toString().isEmpty();
+    }
+
+    // do
+    void adD()
+    {
+        //complet
+        if(ifEmptyFields())
+        {
+            AlertDialog.Builder x = new AlertDialog.Builder(this);
+            x.setMessage("Please complete fill the form data.").setTitle("incomplete data")
+
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            return;
+                        }
+                    })
+
+                    .setIcon(R.drawable.goo)
+                    .setPositiveButtonIcon(getDrawable(R.drawable.yes))
+
+                    .show();
+            return;
+        }
+
+
+        AlertDialog.Builder   x= new AlertDialog.Builder ( this );
+        x.setMessage ( "DO YOU WANT TO ADD Glucose TEST?" ).setTitle ( "Add Glucose test" )
+
+                .setPositiveButton ( "YES_ADD", new DialogInterface.OnClickListener () {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.w ("ADD TEST", "ADD Glucose TEST");
+                        // functions and codes
+                        //complet
+
+                        notification("Glucose Test");
+                        Toast.makeText(getApplicationContext(), "Glucose TEST IS ADD...", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                } )
+
+                .setNegativeButton ( "CANCEL", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { }
+                })
+
+                .setIcon(R.drawable.qus)
+                .setPositiveButtonIcon (getDrawable ( R.drawable.yes))
+                .setNegativeButtonIcon(getDrawable ( R.drawable.no))
+                .show ();
+
+    }
+
+    // clear
+    void cleaR()
+    {
+        //complet
+
+        AlertDialog.Builder   x= new AlertDialog.Builder ( this );
+        x.setMessage ( "DO YOU WANT TO CLEAR FIELDS?" ).setTitle ( "Clear Fields" )
+
+                .setPositiveButton ( "YES_CLEAR", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.w ("CLEAR FIELDS", "Glucose TEST CLEAR FIELDS");
+                        Toast.makeText(getApplicationContext(), "FIELDS IS CLEARD...", Toast.LENGTH_SHORT).show();
+                        // functions and codes
+                        //complet
+                        autoTD.setChecked(false);
+                        autoTD.callOnClick();
+                        datE.setText("YYYY/MM/DD");
+                        timE.setText("HH:MM");
+                        glucose.setText("");
+
+                    }
+                } )
+
+                .setNegativeButton ( "CANCEL", new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { }
+                })
+
+                .setIcon(R.drawable.qus)
+                .setPositiveButtonIcon (getDrawable ( R.drawable.yes))
+                .setNegativeButtonIcon(getDrawable ( R.drawable.no))
+                .show ();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onClick(View v) {
+
+        if (v == add) { adD(); return; }
+        if (v == clear) { cleaR(); return; }
+        if (v == autoTD) { onCheckboxClicked(v); return; }
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+
+        if(v==glucose)
+        {
+            if (!hasFocus) {
+                Log.d("focus", "focus lost");
+                // Do whatever you want here
+            } else {
+                Toast.makeText(getApplicationContext(), " Tap outside edittext to lose focus ", Toast.LENGTH_SHORT).show();
+                Log.d("focus", "focused");
+            }
+
+            return;
+        }
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        //complet
+
+    }
+
+    //
+    // <!-- Clear focus on touch outside for all EditText inputs. "Clear focus input"
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
+    }
+// "Clear focus input" -->
+
+    // notification
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createChannel() {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel x;
+            x = new NotificationChannel(ChannelID, "My  Hi Channel with you", NotificationManager.IMPORTANCE_HIGH);
+
+            NotificationManager man = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            man.createNotificationChannel(x);
+
+
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void notification(String text)
+    {
+        NotificationManager man= (NotificationManager)getSystemService ( NOTIFICATION_SERVICE );
+        NotificationCompat.Builder  note=null;
+
+
+        createChannel();
+
+        NotificationCompat.BigTextStyle bigtext = new NotificationCompat.BigTextStyle ();
+        bigtext.setBigContentTitle ("Test Type:"+text);
+        bigtext.bigText ("Test Date:"+ datE.getText().toString()+ " && Test Time"+timE.getText().toString() );
+        bigtext.setSummaryText ("New  Test ADD");
+
+        note = new NotificationCompat.Builder ( getApplicationContext(),ChannelID )
+                /*.setContentTitle ( "New  Test ADD"  )
+                .setSubText ( "Test Type:"+text
+                        +"\nTest Date:"+ datE.getText().toString()
+                        +"\nTest Time:"+timE.getText().toString()  )
+                .setContentText ("")*/
+                .setOngoing ( false )
+                .setColor ( Color.RED  )
+                .setColorized ( true )
+                .setPriority ( NotificationManager.IMPORTANCE_HIGH )
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setShowWhen ( true )
+                .setUsesChronometer ( true )
+                .setSmallIcon ( R.drawable.icof)
+                .setStyle ( bigtext )
+                .setLargeIcon ( BitmapFactory.decodeResource ( getResources (),R.drawable.icof ) )
+                .setAutoCancel ( true )
+        //.setOnlyAlertOnce(true)
+        //.addAction ( R.drawable.no,"Mark Complete", markCompleteIntent);
+        ;
+
+        man.notify (++Functions.ne, note.build ());
+
+    }
+}
---
+++ app/src/main/java/com/example/healthza/Functions.java
@@ -0,0 +1,139 @@
+package com.example.healthza;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class Functions extends AppCompatActivity{
+
+    public static String dateS = "";
+    public static String timeS = "";
+
+    static    int ne=1;//  Notification ID"Counter"
+
+    //Date
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        private static int Year;
+        private static int Month;
+        private static int Day;
+
+        TextView f;
+
+        public static int getYear() {
+            return Year;
+        }
+
+        public static int getMonth() {
+            return Month;
+        }
+
+        public static int getDay() {
+            return Day;
+        }
+
+        public static void setYear(int year) {
+            Year = year;
+        }
+
+        public static void setMonth(int month) {
+            Month = month;
+        }
+
+        public static void setDay(int day) {
+            Day = day;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            setYear(year);
+            setMonth(month+1);
+            setDay(day);
+            dateS = getYear()+"/"+getMonth()+"/"+getDay();
+            f.setText(dateS);
+            Toast.makeText(this.getContext(), "..."+getYear()+" "+getMonth()+" "+getDay()+"...", Toast.LENGTH_SHORT).show();
+        }
+
+        public DatePickerFragment(TextView g)
+        {
+            f=g;
+        }
+    }
+
+    //Time
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        private static int Hour;
+        private static int Minute;
+
+        TextView f;
+
+        public static int getHour() {
+            return Hour;
+        }
+
+        public static int getMinute() {
+            return Minute;
+        }
+
+        public static void setHour(int hour) {
+            Hour = hour;
+        }
+
+        public static void setMinute(int minute) {
+            Minute = minute;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            setHour(hourOfDay);
+            setMinute(minute);
+            timeS = TimePickerFragment.getHour()+":"+TimePickerFragment.getMinute();
+            f.setText(timeS);
+            Toast.makeText(this.getContext(), "..." + getHour() + " " + getMinute() + "...", Toast.LENGTH_SHORT).show();
+        }
+
+        public TimePickerFragment(TextView g)
+        {
+            f=g;
+        }
+    }
+
+}
