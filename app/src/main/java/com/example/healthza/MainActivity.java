bb711a4b56df1f4b4af7a1fff6615983acdbbbf1
yazan <yazan.nidal@hotmail.com>
2021-03-20 07:06:37 +0200

    2pe

--- app/src/main/java/com/example/healthza/MainActivity.java
+++ app/src/main/java/com/example/healthza/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent I = new Intent(this, newChronicDiseases.class);
                 startActivity(I);
+                //sdd
             }
 }
